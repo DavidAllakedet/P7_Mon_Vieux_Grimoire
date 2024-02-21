068f91594e06173d40bd99aa83ee01c852c45440
David Allakedet <allakedetdavid@gmail.com>
2024-02-21 04:33:24 +0000

    Modifier P7-Dev-Web-livres et backend/Routes/user.js

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userCrts = require('../Controllers/user')
-// const User = require('../models/User'); 
 
 
 router.post('/signup', userCrts.signup );
